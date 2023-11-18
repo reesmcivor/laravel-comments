7b81a7793d7739a37c206d2fb5a6af7dd980b39a
reesmcivor <reesmcivor@hotmail.com>
2023-11-18 23:32:19 +0100

    Added test

---
+++ publish/tests/Feature/Http/Api/CommentsTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ReesMcIvor\Comments\Tests\Feature\Http\Api;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use ReesMcIvor\Labels\Models\Label;
+
+class CommentsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_commend_can_be_created()
+    {
+    }
+}
