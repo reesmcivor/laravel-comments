5ad0c4d907e1e6a639dba161456841ff62787cc6
reesmcivor <reesmcivor@hotmail.com>
2024-05-09 07:00:27 +0100

    fix: added model and other returnable data to allow comments on routines and other models

--- src/Http/Resources/CommentResource.php
+++ src/Http/Resources/CommentResource.php
@@ -11,6 +11,11 @@ class CommentResource extends JsonResource
         return [
             'id' => $this->id,
             'content' => $this->content,
+            'commentable_id' => $this->commentable_id,
+            'commentable_type' => $this->commentable_type,
+            'model' => [
+                'name' => $this->commentable?->name
+            ],
             'files' => new FileResourceCollection($this->files),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at
